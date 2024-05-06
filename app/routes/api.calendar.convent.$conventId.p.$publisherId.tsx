d7bc6c742b2c46fb4851e8a5ed66eecbb8c93670
edgesoft <wicket.programmer@gmail.com>
2024-05-06 14:27:22 +0200

    Update calendar
    fix: #4

--- app/routes/api.calendar.convent.$conventId.p.$publisherId.tsx
+++ app/routes/api.calendar.convent.$conventId.p.$publisherId.tsx
@@ -7,9 +7,7 @@ const prisma = new PrismaClient();
 export const loader: LoaderFunction = async ({ params }) => {
     const { conventId, userId, publisherId } = params;
 
-    const convent = await prisma.convent.findUnique({where: {id: Number(conventId)}})
-    console.log(convent)
-    const publisher = await prisma.publisher.findUnique({where: {id: Number(publisherId)}})
+    const convent = await prisma.convent.findUnique({where: {id: Number(conventId)}});
 
     const publisherSessions = await prisma.publisher.findMany({
         where: {
@@ -24,42 +22,37 @@ export const loader: LoaderFunction = async ({ params }) => {
         }
       });
 
-
-
-
-    // Skapa en ny kalender
     const calendar = ical({ name: convent?.theme});
-
-    console.log(publisherSessions)
-
-    // Lägg till events till kalendern
-    publisherSessions.forEach(p => {
-        console.log(p)
-        const  sessions = p.sessions
-        console.log(sessions)
-
-
-        sessions.forEach(s => {
-          const { date, startHour, startMinutes, stopHour, stopMinutes, theme, identifier } = s.session;
+    for (const p of publisherSessions) {
+        for (const s of p.sessions) {
+          const { id, date, startHour, startMinutes, stopHour, stopMinutes, theme, identifier } = s.session;
           const baseDate = new Date(date);
           const startDate = new Date(baseDate.getFullYear(), baseDate.getMonth(), baseDate.getDate(), startHour, startMinutes);
           const endDate = new Date(baseDate.getFullYear(), baseDate.getMonth(), baseDate.getDate(), stopHour, stopMinutes);
-      
-  
-          calendar.createEvent({
+
+          const sessionPublishers = await prisma.sessionPublisher.findMany({
+            where: {
+              sessionId: id,
+            },
+            include: {
+              publisher: true,
+            }
+          });
+
+          const publisherNames = sessionPublishers.map(sp => sp.publisher.name).join(", ");
+
+          const event = calendar.createEvent({
               start: startDate,
               end: endDate,
               summary: `${theme} (${identifier})`,
-              description: "test",
+              description: publisherNames ? `Ansvariga: ${publisherNames}`: "",
+              timezone: 'Europe/Stockholm'
           });
-        })
-      
-    });
+          event.uid(`convent-${conventId}-${publisherId}`);
+        }
+    }
 
-    // Generera ICS-filen
     const icsData = calendar.toString();
-
-    // Ställ in rätt headers för ICS-filen
     return new Response(icsData, {
         headers: {
             'Content-Type': 'text/calendar',
@@ -67,4 +60,3 @@ export const loader: LoaderFunction = async ({ params }) => {
         }
     });
 };
-
