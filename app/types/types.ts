7d2fda20c26ac0518508299371d49c64db0bcaf8
edgesoft <wicket.programmer@gmail.com>
2024-05-07 00:53:54 +0200

    update types

--- app/types/types.ts
+++ app/types/types.ts
@@ -1,9 +1,24 @@
-import { SessionStep } from "@prisma/client";
+import { Convent, Publisher, Session, SessionStep, User } from "@prisma/client";
 
 export type PositionType = "absolute" | "relative" | "fixed" | "sticky";
+
 export type StepProps = {
-    step: SessionStep,
-    disabled: boolean,
-    onEvent: (id: number, isCompleted: boolean) => void;
-  }
-  
+  step: SessionStep;
+  disabled: boolean;
+  onEvent: (id: number, isCompleted: boolean) => void;
+};
+
+
+export type SessionProps = Omit<Session, ''> & {
+  steps: SessionStep[];
+  publishers: Publisher[]
+}
+
+export type ConventLoaderType = {
+  user: User;
+  convent: Convent;
+  sessions: SessionProps[];
+  env: string;
+};
+
+
