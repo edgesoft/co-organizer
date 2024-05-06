234f09a1934111556ab9faa4cc6086c3398f427d
edgesoft <wicket.programmer@gmail.com>
2024-05-07 00:54:25 +0200

    update groups search

--- app/routes/api.groups.tsx
+++ app/routes/api.groups.tsx
@@ -1,20 +1,39 @@
-import { SessionType } from "@prisma/client";
+import { SessionType, StepType } from "@prisma/client";
 import { ActionFunction, json } from "@remix-run/node";
 import { prisma } from "~/services/db.server";
 
 export const action: ActionFunction = async ({ request }) => {
   const data = await request.json();
 
+
   const sessions = await prisma.session.findMany({
     where: {
-     conventId: Number(data.conventId),
-      groupSessionId: null,
-      OR: [
-        { type: SessionType.TALK },
-        { type: SessionType.PRAYER },
-        { type: SessionType.CHAIR_MAN }
+      conventId: Number(data.conventId),
+      AND: [
+        {
+          OR: [
+            { groupSessionId: null },
+            { groupSessionId: data.groupSessionId }
+          ]
+        },
+        {
+          steps: {
+            some: {
+              stepType: StepType.PODIUM_PRACTICE_DONE
+            }
+          }
+        },
+        {
+          OR: [
+            { type: SessionType.TALK },
+            { type: SessionType.PRAYER },
+            { type: SessionType.CHAIR_MAN }
+          ]
+        }
       ]
-    }
+    },
+    orderBy: [{ startHour: "asc" }, { startMinutes: "asc" }],
   });
+  
   return json(sessions);
 };
--- app/routes/groupSearch.tsx
+++ app/routes/groupSearch.tsx
@@ -1,34 +1,65 @@
+import { SessionType, StepType } from "@prisma/client";
 import { ActionFunction, json } from "@remix-run/node";
 import { prisma } from "~/services/db.server";
 
 export const action: ActionFunction = async ({ request }) => {
-    const formData = await request.formData();
-    const id = formData.get("id")
-    const groupSessionId = formData.get("groupSessionId")
-    
+  const formData = await request.formData();
+  const id = formData.get("id");
+  const groupSessionId = formData.get("groupSessionId");
+
+
+  // get all remaining
+  if (!groupSessionId) {
+    const conventId = formData.get("conventId");
     const sessions = await prisma.session.findMany({
-        where: {
-          groupSessionId: Number(groupSessionId),
-          NOT: {
-            id: Number(id)
-          }
+      where: {
+        groupSessionId: null,
+        conventId: Number(conventId),
+        type: {
+          in: [SessionType.PRAYER, SessionType.TALK, SessionType.CHAIR_MAN],
         },
-        include: {
-            steps: true,
-            publishers: {
-              include: {
-                  publisher: true
-              }
-          }
+        steps: {
+          some: {
+            stepType: StepType.PODIUM_PRACTICE_DONE,
           },
-        orderBy: [
-            { startHour: 'asc' },
-            { startMinutes: 'asc' }
-          ]
-      });
-  
-    return json({sessions});
-  };
+        },
+      },
+      include: {
+        steps: true,
+        publishers: {
+          include: {
+            publisher: true,
+          },
+        },
+      },
+      orderBy: [{ startHour: "asc" }, { startMinutes: "asc" }],
+    });
 
+    return json({ sessions });
+  }
 
-  
+  const sessions = await prisma.session.findMany({
+    where: {
+      groupSessionId: Number(groupSessionId),
+      NOT: {
+        id: Number(id),
+      },
+      steps: {
+        some: {
+          stepType: StepType.PODIUM_PRACTICE_DONE,
+        },
+      },
+    },
+    include: {
+      steps: true,
+      publishers: {
+        include: {
+          publisher: true,
+        },
+      },
+    },
+    orderBy: [{ startHour: "asc" }, { startMinutes: "asc" }],
+  });
+
+  return json({ sessions });
+};
