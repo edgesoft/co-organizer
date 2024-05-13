b0ad9384d72717fbd743247e50c79fb0ebe81d73
edgesoft <wicket.programmer@gmail.com>
2024-05-13 22:17:44 +0200

    Update calendar to include podie practice in description
    fix: #16

--- app/routes/api.calendar.convent.$conventId.p.$publisherId.tsx
+++ app/routes/api.calendar.convent.$conventId.p.$publisherId.tsx
@@ -1,62 +1,114 @@
-import { json, LoaderFunction } from '@remix-run/node';
-import { PrismaClient } from '@prisma/client';
-import ical from 'ical-generator';
+import { json, LoaderFunction } from "@remix-run/node";
+import { PrismaClient, SessionType } from "@prisma/client";
+import ical from "ical-generator";
 
 const prisma = new PrismaClient();
 
 export const loader: LoaderFunction = async ({ params }) => {
-    const { conventId, userId, publisherId } = params;
+  const { conventId, userId, publisherId } = params;
 
-    const convent = await prisma.convent.findUnique({where: {id: Number(conventId)}});
+  const convent = await prisma.convent.findUnique({
+    where: { id: Number(conventId) },
+  });
 
-    const publisherSessions = await prisma.publisher.findMany({
+  const publisherSessions = await prisma.publisher.findMany({
+    where: {
+      id: Number(publisherId),
+    },
+    include: {
+      sessions: {
+        include: {
+          session: true, // Hämta alla detaljer för varje session
+        },
+      },
+    },
+  });
+
+  const calendar = ical({ name: convent?.theme });
+  for (const p of publisherSessions) {
+    for (const s of p.sessions) {
+      const {
+        id,
+        date,
+        startHour,
+        startMinutes,
+        stopHour,
+        stopMinutes,
+        theme,
+        groupSessionId,
+        identifier,
+        type,
+      } = s.session;
+      const baseDate = new Date(date);
+      const startDate = new Date(
+        baseDate.getFullYear(),
+        baseDate.getMonth(),
+        baseDate.getDate(),
+        startHour,
+        startMinutes
+      );
+      const endDate = new Date(
+        baseDate.getFullYear(),
+        baseDate.getMonth(),
+        baseDate.getDate(),
+        stopHour,
+        stopMinutes
+      );
+
+      const sessionPublishers = await prisma.sessionPublisher.findMany({
         where: {
-          id: Number(publisherId),
+          sessionId: id,
         },
         include: {
-          sessions: {
-            include: {
-              session: true, // Hämta alla detaljer för varje session
-            }
-          }
-        }
+          publisher: true,
+        },
       });
 
-    const calendar = ical({ name: convent?.theme});
-    for (const p of publisherSessions) {
-        for (const s of p.sessions) {
-          const { id, date, startHour, startMinutes, stopHour, stopMinutes, theme, identifier } = s.session;
-          const baseDate = new Date(date);
-          const startDate = new Date(baseDate.getFullYear(), baseDate.getMonth(), baseDate.getDate(), startHour, startMinutes);
-          const endDate = new Date(baseDate.getFullYear(), baseDate.getMonth(), baseDate.getDate(), stopHour, stopMinutes);
+      const publisherNames = sessionPublishers
+        .map((sp) => sp.publisher.name)
+        .join(", ");
+      let podiumPracticeDescription = "";
 
-          const sessionPublishers = await prisma.sessionPublisher.findMany({
-            where: {
-              sessionId: id,
+      if (type === SessionType.PODIUM_PRACTICE) {
+        const groupSessions = await prisma.session.findMany({
+          where: {
+            groupSessionId: groupSessionId,
+          },
+          include: {
+            publishers: {
+              include: {
+                publisher: true,
+              },
             },
-            include: {
-              publisher: true,
-            }
-          });
+          },
+        });
 
-          const publisherNames = sessionPublishers.map(sp => sp.publisher.name).join(", ");
-
-          const event = calendar.createEvent({
-              start: startDate,
-              end: endDate,
-              summary: `${theme} (${identifier})`,
-              description: publisherNames ? `Ansvariga: ${publisherNames}`: "",
-              timezone: 'Europe/Stockholm'
-          });
-          event.uid(`convent-${conventId}-${publisherId}-${id}`);
+        podiumPracticeDescription = "\n\nPodieövning:\n";
+        for (const groupSession of groupSessions) {
+           if (groupSession.id !== id) {
+            for (const sp of groupSession.publishers) {
+              podiumPracticeDescription += `${sp.publisher.name}\n${groupSession.theme}(${groupSession.identifier})\n\n`;
+          }
+           }
         }
+      }
+
+      const event = calendar.createEvent({
+        start: startDate,
+        end: endDate,
+        summary: `${theme} (${identifier})`,
+        description: publisherNames ? `Ansvariga: ${publisherNames}${podiumPracticeDescription}` : podiumPracticeDescription,
+        timezone: "Europe/Stockholm",
+      });
+      event.uid(`convent-${conventId}-${publisherId}-${id}`);
     }
+  }
 
-    const icsData = calendar.toString();
-    return new Response(icsData, {
-        headers: {
-            'Content-Type': 'text/calendar',
-            'Content-Disposition': `attachment; filename="convent-${conventId}-${publisherId}.ics"`
-        }
-    });
+  const icsData = calendar.toString();
+  return new Response(icsData, {
+    headers: {
+      "Content-Type": "text/calendar",
+      "Content-Disposition": `attachment; filename="convent-${conventId}-${publisherId}.ics"`,
+    },
+  });
 };
